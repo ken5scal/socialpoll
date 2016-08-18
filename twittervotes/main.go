ca0a032a20380996d133b6af94c4ea8798c0a949
ken5scal <kengoscal@gmail.com>
2016-08-19 01:59:11 +0900

    Add Specification

--- twittervotes/main.go
+++ twittervotes/main.go
@@ -1,4 +1,8 @@
 package main
 
 func main() {
+	// read all polling result and retrieve all polls
+	// from $options array in each document using mgo via MongoDB
+
+	// Use Twitter streaming api to manage session and search for tweet responding to the polls
 }
