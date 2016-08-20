757a14a3ad6115742e89391a0bf741753f4e054d
ken5scal <kengoscal@gmail.com>
2016-08-20 18:17:11 +0900

    Format

--- twittervotes/main.go
+++ twittervotes/main.go
@@ -1,14 +1,33 @@
 package main
 
-func main() {
-	// read all polling result and retrieve all polls
-	// from $options array in each document using mgo via MongoDB
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+)
 
-	// Use Twitter streaming api to manage session and search for tweet responding to the polls
 
-	// Send search result tweet with selection to NSQ
+// read all polling result and retrieve all polls
+// from $options array in each document using mgo via MongoDB
+var db *mgo.Session
 
-	// Periodically fetch inspecing indexes from MongoDB and renew connections to Twitter
-
-	// When Ctrl + C, then finish program
+func dialdb() error {
+	var err error
+	log.Println("Dialing MongoDB: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
 }
+
+func closedb() {
+	db.Close()
+	log.Println("Closed db connection")
+}
+
+
+// Use Twitter streaming api to manage session and search for tweet responding to the polls
+
+// Send search result tweet with selection to NSQ
+
+// Periodically fetch inspecing indexes from MongoDB and renew connections to Twitter
+
+// When Ctrl + C, then finish program
+
