b13a3eca025c5e1c6ecf892b4e7f147a61e8cccf
ken5scal <kengoscal@gmail.com>
2016-08-22 08:48:26 +0900

    Add method handles nsq messages

--- counter/main.go
+++ counter/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"log"
 	"gopkg.in/mgo.v2"
+	"sync"
+	"github.com/bitly/go-nsq"
 )
 
 var fatalErr error
@@ -33,6 +35,32 @@ func main() {
 		log.Println("Disconnecting db...")
 		db.Close()
 	}()
-
 	pollData := db.DB("ballots").C("polls")
+
+	var countsLock sync.Mutex // map and lock. avoid read/write conflicts by multiple goroutine to a map
+	var counts map[string]int
+	log.Println("Connecting NSQ...")
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return // ensures to finish main (call defer funcs)
+	}
+
+	// Handle events receiving a message from NSQ
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts ==nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+
+	// Connecting to NSQ Service
+	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
 }
