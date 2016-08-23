f5ca38cd5a15dc6f6775067c8db310e85a6abd6b
ken5scal <kengoscal@gmail.com>
2016-08-23 09:16:40 +0900

    Add api related method

--- api/main.go
+++ api/main.go
@@ -1,4 +1,20 @@
 package main
 
+import "net/http"
+
 func main() {
+}
+
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !isValidAPIKey(r.URL.Query().Get("key")) {
+			respondErr(w, r, http.StatusUnauthorized, "Invalid API Key")
+			return
+		}
+		fn(w, r)
+	}
+}
+
+func isValidAPIKey(key string) bool {
+	return key == "abc123"
 }
