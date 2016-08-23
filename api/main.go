a8af30d0d59c841dd2d6fd26966c6b483cc297a5
ken5scal <kengoscal@gmail.com>
2016-08-23 09:19:29 +0900

    .

--- api/main.go
+++ api/main.go
@@ -5,6 +5,7 @@ import "net/http"
 func main() {
 }
 
+// Wrap HandlerFunc
 func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if !isValidAPIKey(r.URL.Query().Get("key")) {
