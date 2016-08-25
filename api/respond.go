0cb45dc3bbdfd1ca3465376581670fbf4f83970f
ken5scal <kengoscal@gmail.com>
2016-08-25 09:02:42 +0900

    encode/decode http request body

---
+++ api/respond.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+func decodeBody(r *http.Request, v interface{}) error {
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func encodeBody(w http.ResponseWriter, r *http.Request, v interface{}) error {
+	return json.NewEncoder(w).Encode(v)
+}
+
+
