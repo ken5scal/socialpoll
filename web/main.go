041c514f63fcc5542b6a961087102fc8ee1e4afa
ken5scal <kengoscal@gmail.com>
2016-08-29 08:42:52 +0900

    Add public folder

--- web/main.go
+++ web/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"flag"
+	"net/http"
+	"log"
+)
+
 func main() {
+	var addr = flag.String("addr", ":8081", "Web Address")
+	flag.Parse()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", http.StripPrefix("/", http.FileServer(http.Dir("public"))))
+	log.Println("Web site address: ", *addr)
+	http.ListenAndServe(*addr, mux)
 }
